5df4044d3609684f78d15dfe23fc075c8263293f
Luq <123>
2020-04-03 12:21:13 +0800

    新建gym项目

--- gym/src/main/java/com/zhiyou100/gym/GymApplication.java
+++ gym/src/main/java/com/zhiyou100/gym/GymApplication.java
@@ -1,11 +1,12 @@
 package com.zhiyou100.gym;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.zhiyou100.gym.mapper")
 public class GymApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GymApplication.class, args);
     }
